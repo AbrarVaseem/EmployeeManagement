a40620937304e0a8c8c76e98aaa5978c021caef0
AbrarVaseem <50957899+AbrarVaseem@users.noreply.github.com>
2024-12-29 16:02:25 +0530

    All CRUD Requests Added

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -1,5 +1,6 @@
 const Employee = require("../models/Employee");
 
+// Create Employee
 const createEmployee = async (req, res) => {
   try {
     const { name, email, phone, city } = req.body;
@@ -12,4 +13,54 @@ const createEmployee = async (req, res) => {
   }
 };
 
-module.exports = { createEmployee };
+// Get All Employee
+const getAllEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+  } catch (e) {
+    res.status(500).json({ message: `${err} Failed to Get Employees` });
+  }
+};
+
+// Get Single Employee
+const getEmployeeById = async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id);
+    res.status(200).json(employee);
+  } catch (e) {
+    res.status(500).json({ message: `${err} Failed to Get Employee ID` });
+  }
+};
+
+// Update Employee Data
+const updateEmployee = async (req, res) => {
+  try {
+    const { name, email, phone, city } = req.body;
+    const employee = await Employee.findByIdAndUpdate(req.params.id, {
+      name,
+      email,
+      phone,
+      city,
+    });
+    if (!employee) {
+      return res.status(404).json({ message: `Employee ID Not Found` });
+    }
+    console.log(employee)
+    res.status(200).json(employee);
+  } catch (e) {
+    res.status(500).json({ message: `Failed to Update Employee` });
+  }
+};
+
+// Delete Employee Id
+const deleteEmployee = async (req, res) => {
+  try {
+    const deleteEmployee = await Employee.findByIdAndDelete(req.params.id);
+    res.status(204).json(deleteEmployee);
+  } catch (e) {
+    res.status(500).json({ message: `Failed to Delete Employee` });
+  }
+};
+
+module.exports = { createEmployee, getAllEmployees, getEmployeeById, updateEmployee, deleteEmployee };
--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 const EmployeeController = require("../controllers/EmployeeController");
 router.post("/create-employee", EmployeeController.createEmployee);
+router.get("/get-employees", EmployeeController.getAllEmployees);
+router.get("/get-employee/:id", EmployeeController.getEmployeeById);
+router.put("/update-employee/:id", EmployeeController.updateEmployee);
+router.delete("/delete-employee/:id", EmployeeController.deleteEmployee);
 
 module.exports = router;
